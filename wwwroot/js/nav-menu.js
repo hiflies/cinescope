fe26f79e5f59fd729e8e30a6298bd5a2651ab33b
Halime Uçar Türk <halimeucarturk@gmail.com>
2026-06-05 18:34:31 +0200

    add toggle dark mode

--- wwwroot/js/nav-menu.js
+++ wwwroot/js/nav-menu.js
@@ -1,3 +1,25 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+
+function updateThemeIcon() {
+    const isLight = document.documentElement.classList.contains('light');
+    themeIcon.textContent = isLight ? 'dark_mode' : 'light_mode';
+}
+
+updateThemeIcon();
+
+themeToggle.addEventListener('click', () => {
+    const isLight = document.documentElement.classList.contains('light');
+    if (!isLight) {
+        document.documentElement.classList.add('light');
+        localStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.classList.remove('light');
+        localStorage.setItem('theme', 'dark');
+    }
+    updateThemeIcon();
+});
+
 const nav = document.getElementById('top-nav');
 const navLoginBar = document.getElementById('nav-login-bar');
 window.addEventListener('scroll', () => {
